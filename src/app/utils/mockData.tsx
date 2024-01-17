381e673f89e17bf1e81af45c40bc2779e7d17d81
Djibril <najidjibril@gmail.com>
2024-01-17 19:38:32 +0100

    feat : create utils folder for all functions or data

---
+++ src/app/utils/mockData.tsx
@@ -0,0 +1,42 @@
+import UserData from "../models/UserData";
+
+// Tous les users présents dans le seed de départ
+export const currentUser1: UserData = {
+  userName: "Jonhy",
+  firstName: "John",
+  lastName: "Doe",
+  matricule: "123456",
+  secretCode: "abc123",
+};
+
+export const currentUser2: UserData = {
+  userName: "Janou",
+  firstName: "Jane",
+  lastName: "Smith",
+  matricule: "789012",
+  secretCode: "abc456",
+};
+
+export const currentUser3: UserData = {
+  userName: "Aliçou",
+  firstName: "Alice",
+  lastName: "Johnson",
+  matricule: "567890",
+  secretCode: "abc789",
+};
+
+export const currentUser4: UserData = {
+  userName: "Bobby",
+  firstName: "Bob",
+  lastName: "Miller",
+  matricule: "345678",
+  secretCode: "xyz789",
+};
+
+export const currentUser5: UserData = {
+  userName: "Emmou",
+  firstName: "Emma",
+  lastName: "Watson",
+  matricule: "901234",
+  secretCode: "xyz123",
+};
