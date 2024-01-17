fbd524698a8446605809fcd99c0ae56fc34a33a9
Djibril <najidjibril@gmail.com>
2024-01-17 16:09:02 +0100

    improve code

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,27 +1,15 @@
 import Navbar from "../components/Layout/Navbar";
 
-import ViewRequest from "../components/Home/ViewRequest";
 import CreateRequest from "../components/Home/CreateRequest";
 import FindRoute from "../components/Home/FindRoute";
+import ViewRequest from "../components/Home/ViewRequest";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1920"
-        height="275"
-        viewBox="0 0 1920 275"
-        fill="none"
-        className="h-full w-full"
-      >
-        <path
-          d="M0 160.457L80 181.817C160 203.579 320 245.699 480 246C640 245.699 800 203.579 960 176.502C1120 149.425 1253.5 275.301 1413.5 275C1573.5 275.301 1760 149.425 1840 155.141L1920 160.457V0H0V160.457Z"
-          fill="#E7EEF6"
-        />
-      </svg>
-      <div className="flex gap-8">
+
+      <div className="border-2 border border-red-500">
         <ViewRequest />
         <CreateRequest />
         <FindRoute />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+import CreateRequest from "./components/Home/CreateRequest";
+import FindRoute from "./components/Home/FindRoute";
+import ViewRequest from "./components/Home/ViewRequest";
 import Navbar from "./components/Layout/Navbar";
 
 export default function Home() {
-  return <Navbar />;
+  return (
+    <div className="">
+      <Navbar />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1920"
+        height="275"
+        viewBox="0 0 1920 275"
+        fill="none"
+        className="h-full w-full"
+      >
+        <path
+          d="M0 160.457L80 181.817C160 203.579 320 245.699 480 246C640 245.699 800 203.579 960 176.502C1120 149.425 1253.5 275.301 1413.5 275C1573.5 275.301 1760 149.425 1840 155.141L1920 160.457V0H0V160.457Z"
+          fill="#E7EEF6"
+        />
+      </svg>
+      <div className="flex flex-col xl:flex-row gap-8">
+        <ViewRequest />
+        <CreateRequest />
+        <FindRoute />
+      </div>
+    </div>
+  );
 }
