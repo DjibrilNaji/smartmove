69f403e709d44b475f5d458cd9a5a986661289ae
LouisLagrange1 <ljblagrange@gmail.com>
2024-01-18 14:44:49 +0100

    create component

---
+++ src/app/components/ResultRoute/Date.tsx
@@ -0,0 +1,15 @@
+export default function Date() {
+  return (
+    <div className="flex gap-4 justify-center items-center">
+      <span className="w-[130px] h-[38px] p-[9px 27.38px 9px 27.72px] border-[1px] border-[#838383] flex justify-center items-center rounded-md text-[#838383]">
+        Mar 13 Fév
+      </span>
+      <span className="w-[130px] h-[44px] p-[12px 27.21px 12px 27.54px]  flex justify-center items-center rounded-md  bg-white text-[#208CFB] shadow-md">
+        Mer 14 Fév
+      </span>
+      <span className="w-[130px] h-[38px] p-[9px 27.38px 9px 27.72px] border-[1px] border-[#838383] flex justify-center items-center rounded-md text-[#838383]">
+        Jeu 15 Fév
+      </span>
+    </div>
+  );
+}
